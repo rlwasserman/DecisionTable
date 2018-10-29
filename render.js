feec3eb74530c29e22866d66a2dd945206aacdde
Richard Wasserman <rlwasserman@gmail.com>
2018-10-29 19:03:06 -0400

    Add files via upload

---
+++ render.js
@@ -0,0 +1,10 @@
+
+function CountMultivateConditions() {
+	// Get count of items multivariate collections 
+	return 0;
+}
+
+function SumOfMultivateTests() {
+    // Get count of each multivariate collection items 
+	return 0;
+}
