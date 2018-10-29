75ea8339f23ae23b624f10b346f17e48a077c7bb
Richard Wasserman <rlwasserman@gmail.com>
2018-10-29 18:58:22 -0400

    js files to work with collections of conditions

---
+++ condition2value.js
@@ -0,0 +1,10 @@
+
+function CountOfBooleanConditions() {
+    // Get count of each 2 variable collection items 
+	return 0;
+}
+
+function SumOfBooleanConditions() {
+    // Get count of each multivariate collection items 
+	return 2^CountOfBooleanConditions;
+}
